0a7dd8a3d0b8c030035b1a9e34b6bf012fb95c44
PetrovDanil <Tyuiu.PetrovDR@outlook.com>
2025-10-17 00:49:04 +0500

    task1

---
+++ Tyuiu.PetrovDR.Sprint2.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.PetrovDR.Sprint2.Task1.V27.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint2.Task1.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+
+            int a = 654;
+            int b = 325;
+            int c = 154;
+            int d = 333;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { false, false, true, false, true, false };
+
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint2.Task1.V27/Program.cs
@@ -0,0 +1,66 @@
+﻿using Tyuiu.PetrovDR.Sprint2.Task1.V27.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint2.Task1.V27
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            void PrintCenteredLine(string text, int totalWidth)
+            {
+                var padding = totalWidth - text.Length - 2;
+                Console.WriteLine($"* {text}{new string(' ', padding)}*");
+            }
+            var width = 75;
+
+
+
+            DataService ds = new DataService();
+
+            int a = 654;
+            int b = 325;
+            int c = 154;
+            int d = 333;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            PrintCenteredLine("Спринт #2 | Выполнил: Петров Д. Р. | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("Спринт #2", width);
+            PrintCenteredLine("Тема: Алгоритмы разветвляющейся структуры", width);
+            PrintCenteredLine("Задание #1", width);
+            PrintCenteredLine("Вариант #27", width);
+            PrintCenteredLine("Выполнил: Петров Данил Романович | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("УСЛОВИЕ:", width);
+            PrintCenteredLine("Написать программу из операций сравнений и логических операций,", width);
+            PrintCenteredLine("а также арифметических выражений, которая вернет", width);
+            PrintCenteredLine("логическую последовательность(массив):", width);
+            PrintCenteredLine("(False, False, True, False, True, False),", width);
+            PrintCenteredLine("при a = 654, b = 325, c = 154, d = 333", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("A = " + a, width);
+            PrintCenteredLine("B = " + b, width);
+            PrintCenteredLine("C = " + c, width);
+            PrintCenteredLine("D = " + d, width);
+
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+            Console.WriteLine(new string('*', width));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
