6e0fd3070f36eeb809311cff46741d9561a7c5a8
PetrovDanil <Tyuiu.PetrovDR@outlook.com>
2025-10-18 01:10:09 +0500

    task3

---
+++ Tyuiu.PetrovDR.Sprint2.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.PetrovDR.Sprint2.Task3.V8.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint2.Task3.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double wait = -4.588;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+
+        [TestMethod]
+        public void ValidCalculate2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double wait = 0.75;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+
+        [TestMethod]
+        public void ValidCalculate3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double wait = 0.25;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+
+        [TestMethod]
+        public void ValidCalculate4()
+        {
+            DataService ds = new DataService();
+            double x = -13;
+            double wait = -142.615;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint2.Task3.V8/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.PetrovDR.Sprint2.Task3.V8.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint2.Task3.V8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            void PrintCenteredLine(string text, int totalWidth)
+            {
+                var padding = totalWidth - text.Length - 2;
+                Console.WriteLine($"* {text}{new string(' ', padding)}*");
+            }
+            var width = 75;
+
+
+
+            DataService ds = new DataService();
+
+
+            PrintCenteredLine("Спринт #2 | Выполнил: Петров Д. Р. | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("Спринт #2", width);
+            PrintCenteredLine("Тема: Алгоритмы разветвляющейся структуры", width);
+            PrintCenteredLine("Задание #3", width);
+            PrintCenteredLine("Вариант #8", width);
+            PrintCenteredLine("Выполнил: Петров Данил Романович | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("УСЛОВИЕ:", width);
+            PrintCenteredLine("Написать программу, которая вычисляет требуемое значение функции Y", width);
+            PrintCenteredLine("с использованием вложенных оператор if-else, где пользователь", width);
+            PrintCenteredLine("вводит значение переменной X с клавиатуры. Округлить полученное", width);
+            PrintCenteredLine("значение до трех знаков после запятой", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+            Console.WriteLine(new string('*', width));
+
+
+            Console.WriteLine("Введите значeние X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+            Console.WriteLine(new string('*', width));
+
+            Console.WriteLine("Значение функции равно " + ds.Calculate(x));
+
+            Console.ReadKey();
+        }
+    }
+}
